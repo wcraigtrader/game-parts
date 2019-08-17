6d2f742771d7af0df2a09ecc703436e4655a7402
W. Craig Trader <craig@trader.name>
2019-08-17 18:49:37 -0400

    Update Makefiles to auto-calculate dependencies, and generate all-inclusive SCAD files.

---
+++ bin/scad-dependencies.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+import logging
+import os
+import re
+import sys
+
+LOG = logging.getLogger('scad')
+
+LIBRARIES = []
+
+INCLUDE = re.compile('^include <(.*)>;$')
+
+
+def find_dependencies(src_path):
+    dependencies = []
+    canonical = os.path.abspath(src_path)
+    if canonical not in LIBRARIES:
+        LOG.info("Expanding %s", canonical)
+        LIBRARIES.append(canonical)
+        with open(canonical, 'r') as src_file:
+            here = os.path.curdir
+            os.chdir(os.path.dirname(canonical))
+
+            for count, line in enumerate(src_file):
+                match = INCLUDE.match(line)
+                if match:
+                    filename = match.group(1)
+                    dependencies.append(find_dependencies(filename))
+
+            os.chdir(here)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
+
+    if len(sys.argv) < 2:
+        LOG.error("usage: %s <source>", sys.argv[0])
+        sys.exit(1)
+
+    src = sys.argv[1]
+
+    if not os.path.exists(src):
+        LOG.error("Unable to locate source: %s", src)
+        sys.exit(2)
+
+    find_dependencies(src)
+
+    sys.stdout.write('\n'.join(LIBRARIES) + '\n')
---
+++ bin/scad-expand.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+
+import logging
+import os
+import re
+import sys
+
+LOG = logging.getLogger('scad')
+
+LIBRARIES = []
+
+INCLUDE = re.compile('^include <(.*)>;$')
+
+
+def expand_path(src_path, dst_file):
+    canonical = os.path.abspath(src_path)
+    if canonical not in LIBRARIES:
+        LOG.info("Expanding %s", canonical)
+        LIBRARIES.append(canonical)
+        with open(canonical, 'r') as src_file:
+            here = os.path.curdir
+            os.chdir(os.path.dirname(canonical))
+
+            for count, line in enumerate(src_file):
+                match = INCLUDE.match(line)
+                if match:
+                    filename = match.group(1)
+                    expand_path(filename, dst_file)
+                else:
+                    dst_file.write(line)
+
+            os.chdir(here)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
+
+    if len(sys.argv) < 3:
+        LOG.error("usage: %s <source> <destination>", sys.argv[0])
+        sys.exit(1)
+
+    src = sys.argv[1]
+    dst = sys.argv[2]
+
+    if not os.path.exists(src):
+        LOG.error("Unable to locate source: %s", src)
+        sys.exit(2)
+
+    with open(dst, 'w') as output:
+        expand_path(src, output)
